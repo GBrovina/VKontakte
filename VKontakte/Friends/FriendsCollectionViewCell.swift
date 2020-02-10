ab3cb2086afab9b50daf90cc30e3fc66cd9a8dcc
GBrovina <bunny.brovina@yandex.ru>
2020-02-10 21:40:12 +0300

    Second2

---
+++ VKontakte/Friends/FriendsCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  FriendsCollectionViewCell.swift
+//  VKontakte
+//
+//  Created by Галина  Бровина  on 10.02.2020.
+//  Copyright © 2020 Галина  Бровина . All rights reserved.
+//
+
+import UIKit
+
+
+
+class FriendsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var friendsPhoto: UIImageView!
+    
+    
+    
+}
